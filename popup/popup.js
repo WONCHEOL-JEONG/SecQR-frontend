36f29d1f3197c4e94da1a21c4bfc53241b199c8b
xininny <xininny@gmail.com>
2024-03-13 00:30:29 +0900

    SecQR

---
+++ popup/popup.js
@@ -0,0 +1,16 @@
+// 이 부분에 버튼 클릭에 대한 동작을 추가할 수 있습니다.
+document.getElementById('qrButton').addEventListener('click', function () {
+    // 버튼 1에 대한 동작
+});
+
+document.getElementById('button2').addEventListener('click', function () {
+    // 버튼 2에 대한 동작
+});
+
+document.getElementById('button3').addEventListener('click', function () {
+    // 버튼 3에 대한 동작
+});
+
+document.getElementById('button4').addEventListener('click', function () {
+    // 버튼 4에 대한 동작
+});
