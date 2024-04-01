efae67adbca8407824e759421cfd9294b2deae8a
sbeeno <121777732+sbeeno@users.noreply.github.com>
2024-04-02 02:57:31 +0900

    first

---
+++ SecQR-frontend/popup/popup.js
@@ -0,0 +1,20 @@
+document.addEventListener('DOMContentLoaded', function () {
+    document.querySelector('.scan-button').addEventListener('click', function () {
+        window.location.href = 'newpage.html';
+    });
+});
+
+
+
+document.getElementById('button2').addEventListener('click', function () {
+    // 버튼 2에 대한 동작
+});
+
+document.getElementById('button3').addEventListener('click', function () {
+    // 버튼 3에 대한 동작
+});
+
+document.getElementById('button4').addEventListener('click', function () {
+    // 버튼 4에 대한 동작
+});
+
